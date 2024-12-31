54411681ef3cea0ad010c56d3734c0b06935c970
CircusHan <smcrony010@gmail.com>
2024-12-31 14:11:27 +0900

    initial commit

---
+++ 13_RomanToInt.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sum = 0
+        i = 0
+        roman_d = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        for i in range(len(s)) :
+            if i+1 < len(s) and roman_d[s[i]] < roman_d[s[i+1]]:
+                sum -= roman_d[s[i]]
+            else :
+                sum += roman_d[s[i]]
+        return sum
